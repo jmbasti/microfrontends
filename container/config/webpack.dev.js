f9492388cacaf4b711dd3a8eb92d7a06fb40f134
jmbasti <markchristopherdauz@gmail.com>
2021-06-28 08:13:55 +0800

    initial commit

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,33 @@
+const { merge } = require("webpack-merge");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        // From Marketing SPA
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        // From Dashboard SPA
+        // From Auth SPA
+      },
+      // shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
