3c8fa15815494a788fac0b11ad2d8333ea347aca
jmbasti <markchristopherdauz@gmail.com>
2021-06-30 07:54:09 +0800

    css prod prefix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./Header";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-      {/* Auth SPA */}
-      {/* Dashboard SPA */}
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        {/* Marketing SPA */}
+        <MarketingApp />
+        {/* Auth SPA */}
+        {/* Dashboard SPA */}
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
