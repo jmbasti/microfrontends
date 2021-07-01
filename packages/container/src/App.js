593deb0373071fb6faf840196d92a95094e0c6ac
jmbasti <markchristopherdauz@gmail.com>
2021-07-01 15:44:37 +0800

    final app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,30 +1,31 @@
-import React, { lazy, Suspense, useState, useEffect } from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
-} from '@material-ui/core/styles';
-import { createBrowserHistory } from 'history';
+} from "@material-ui/core/styles";
+import { createBrowserHistory } from "history";
 
-import Progress from './components/Progress';
-import Header from './components/Header';
+import Progress from "./components/Progress";
+import Header from "./components/Header";
 
-const MarketingLazy = lazy(() => import('./components/MarketingApp'));
-const AuthLazy = lazy(() => import('./components/AuthApp'));
-const DashboardLazy = lazy(() => import('./components/DashboardApp'));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'co',
+  productionPrefix: "co",
 });
 
 const history = createBrowserHistory();
 
+//
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   useEffect(() => {
     if (isSignedIn) {
-      history.push('/dashboard');
+      history.push("/dashboard");
     }
   }, [isSignedIn]);
 
